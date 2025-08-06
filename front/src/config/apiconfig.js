07d5e651e4ad8d980cda68c5836ea319f67b2bf4
Himanshu Singh <138711578+himansh025@users.noreply.github.com>
2025-08-06 23:47:04 +0530

    Update apiconfig.js

--- front/src/config/apiconfig.js
+++ front/src/config/apiconfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const apiUrl = import.meta.env.VITE_API_URL;
 
-const apiUrl = "http://localhost:5000"
+const apiUrl = "https://task-back-sable.vercel.app"
 
 const axiosInstance = axios.create({
   baseURL: `${apiUrl}/api`,
